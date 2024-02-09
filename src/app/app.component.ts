d97519623d44140d555f23b168ea535727bfb89b
IsaacQH <isaacqwert@gmail.com>
2024-02-08 23:46:13 -0600

    Creation of components and routes configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+//Import component:
+import { AboutComponent} from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  //Remember import necessary
+  imports: [CommonModule, RouterOutlet, AboutComponent, ContactComponent, CreateComponent, ProjectsComponent, ErrorComponent,RouterModule],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Import component:
+import { AboutComponent} from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
+
+//Assigning Routes:These are the routes that the url is going to use and the assingned component
+export const routes: Routes = [
+    {path: '', component: AboutComponent},
+    {path: 'about-me', component: AboutComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'create-project', component: CreateComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', component: ErrorComponent},
+];
