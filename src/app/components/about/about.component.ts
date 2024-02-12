a74093f2e354e39640a3eaf26766ab24aef66fbe
IsaacQH <isaacqwert@gmail.com>
2024-02-12 03:27:24 -0600

    About me styling of HTML/CSS and creation of basic class for the information

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  public title:string         //Title initializer as public as string
+  public subtitle:string         //subtitle initializer as public as string
+  public web:string         //web initializer as public as string
+
+  constructor(){
+    this.title = "Isaac Quintero Higuera" //Title declaration
+    this.subtitle = "Software developer" //subtitle declaration
+    this.web = "https://github.com/IsaacQH"  //web declaration
+  }
+
+  ngOnInit() {
+  }
 }
