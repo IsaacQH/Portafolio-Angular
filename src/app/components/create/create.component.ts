0a1d3cce28474ccb5ade196118a11eb02502fcd4
IsaacQH <isaacqwert@gmail.com>
2024-02-14 00:35:45 -0600

    Create POST method and adding success or failed status

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -17,12 +17,14 @@ import { CommonModule } from '@angular/common';
 export class CreateComponent implements OnInit{
   public title: string; // Title for the component
   public project: Project; // Project object
+  public status:string;  //Status of the post
 
   constructor(
     private _projectService: ProjectService
   ){
     this.title = "Create project"; // Sets the title
     this.project = new Project("","","","","", 0,""); // Initializes a new Project object with default values
+    this.status = ""      //declararion of default status
   }
 
   ngOnInit() {
@@ -30,6 +32,23 @@ export class CreateComponent implements OnInit{
   }
 
   onSubmit(form:any):any{
-    console.log(this.project)
+    console.log(this.project) //Prints on console the project objects
+    this._projectService.saveProject(this.project).subscribe(    //Captures and suscribes the project info on the db
+      response => {
+        if(response.project){
+          this.status = "SUCCESS";       //declararion of status is true so the message of HTML appears
+          console.log("This is the result of POST")
+          console.log(response)      //Prints the object project as a full JSON whith id assigned
+          form.reset()
+        }else{
+          this.status = "FAILED";       //declararion of status is false, so the message dont appear
+        }
+        
+      },
+      error =>{
+        console.log("There was an error")   //Prints the error if ther is sone
+        console.log(<any>error)   //Prints the error if ther is sone
+      }
+    )
   }
 }
--- src/app/services/projecs.service.ts
+++ src/app/services/projecs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Project} from "../models/project";
 import { global } from "./global";
@@ -17,4 +17,11 @@ export class ProjectService{
     testService(){
         return "Checking for Project Service"
     }
+
+    saveProject(project: Project):Observable<any>{
+        let params = JSON.stringify(project)     //Converts the object to a string JSON
+        let headers = new HttpHeaders().set('Content-type', 'application/json')  //Headers condifuration that send the json
+                                          //URL where the post is required, post on the backend
+        return this._http.post(this.url + 'saveProject', params, {headers:headers})  //Creates a POST and puts the params on the url and organize the headers por http
+      }
 }
